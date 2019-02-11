ddd87313568b7cc8367d0da9d328bd27957285c7
live.comrohmat661 <rohmat661@gmail.com>
2019-02-11 20:11:31 +0700

    explorasi

---
+++ omdb hasil percobaan xamarin.form/omdb/omdb/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using omdb.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace omdb
+{
+	public partial class MainPage : ContentPage
+	{
+        MainViewModel mainViewModel;
+		public MainPage()
+		{
+			InitializeComponent();
+            mainViewModel = new MainViewModel();
+        }
+        private async void ListView_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if(e == null || e.Item == null)
+            {
+                await DisplayAlert("Error", "List item is null!", "OK");
+                return;
+            }
+            var content = e.Item as Content;            
+            var contentPage = new ContentViewPage(mainViewModel, content);
+            await Application.Current.MainPage.Navigation.PushAsync(contentPage);           
+        }
+    }
+}
