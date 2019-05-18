a4fef0d49efc15132d71e133c6330fb50713ee59
Rohmat Dasuki <dasukirohmat@gmail.com>
2019-05-18 07:12:23 +0700

    update

--- InnStant_v0/app/src/main/java/com/example/innstant/MainActivity.java
+++ InnStant_v0/app/src/main/java/com/example/innstant/MainActivity.java
@@ -60,10 +60,12 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.notify) {
             return true;
         }
-
+        if(id == R.id.message){
+            return  true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
