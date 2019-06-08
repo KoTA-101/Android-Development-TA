5582e313438ef2200fc99dc4b5ecfcc382f20108
Rohmat Dasuki <dasukirohmat@gmail.com>
2019-06-09 05:53:42 +0700

    fixing

--- InnStant_v0/app/src/main/java/com/example/innstant/ui/RentStatus/ReservedActivity.java
+++ InnStant_v0/app/src/main/java/com/example/innstant/ui/RentStatus/ReservedActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.innstant.R;
+
 public class ReservedActivity extends AppCompatActivity {
 
     @Override
