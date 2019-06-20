6e55ce625f0040a74d85daa7c363e0da05c7921e
Rohmat Dasuki <dasukirohmat@gmail.com>
2019-06-20 21:43:54 +0700

    update progress

--- InnStant_v0/app/src/main/java/com/example/innstant/ui/DashboardActivity.java
+++ InnStant_v0/app/src/main/java/com/example/innstant/ui/DashboardActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +14,24 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.innstant.R;
+import com.example.innstant.data.model.comment;
 import com.example.innstant.ui.Dashboard.DashboardMessageActivity;
 import com.example.innstant.ui.Dashboard.DashboardNotificationActivity;
 import com.example.innstant.ui.Rent.RentRoomActivity;
 import com.google.android.material.navigation.NavigationView;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -47,8 +61,40 @@ public class DashboardActivity extends AppCompatActivity
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         navView.setNavigationItemSelectedListener(this);
-        setSupportActionBar(toolbar); 
+        setSupportActionBar(toolbar);
+
+        TestAPI();
     }
+
+    public void TestAPI(){
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://my-json-server.typicode.com/typicode/demo/comments";
+        Gson gson = new GsonBuilder().create();
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Toast.makeText( DashboardActivity.this,"Response is: "+ response,Toast.LENGTH_LONG).show();
+                        ArrayList<comment> coment = gson.fromJson(response, new TypeToken<List<comment>>(){}.getType());
+                       Toast.makeText( DashboardActivity.this,coment.get(0).getBody(),Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText( DashboardActivity.this, error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+
+    }
+
 
     @Override
     public void onBackPressed() {
