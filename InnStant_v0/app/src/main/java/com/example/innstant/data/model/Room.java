02a2f7a78a8b7d6e8485c9ae1aa1e6379e27d2c5
Rohmat Dasuki <dasukirohmat@gmail.com>
2019-06-24 20:29:50 +0700

    update model

--- InnStant_v0/app/src/main/java/com/example/innstant/data/model/Room.java
+++ InnStant_v0/app/src/main/java/com/example/innstant/data/model/Room.java
@@ -1,4 +1,103 @@
 package com.example.innstant.data.model;
 
 public class Room {
+     private String name;
+     private String ownerId;
+     private String type;
+     private String location;
+     private Double latitude;
+     private Double longitude;
+     private String[] amenities;
+     private String description;
+     private String price;
+     private Double dpPercentage;
+     private String photos;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String[] getAmenities() {
+        return amenities;
+    }
+
+    public void setAmenities(String[] amenities) {
+        this.amenities = amenities;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public Double getDpPercentage() {
+        return dpPercentage;
+    }
+
+    public void setDpPercentage(Double dpPercentage) {
+        this.dpPercentage = dpPercentage;
+    }
+
+    public String getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(String photos) {
+        this.photos = photos;
+    }
 }
--- InnStant_v0/app/src/main/java/com/example/innstant/data/model/User.java
+++ InnStant_v0/app/src/main/java/com/example/innstant/data/model/User.java
@@ -1,4 +1,105 @@
 package com.example.innstant.data.model;
 
+import android.graphics.Bitmap;
+
 public class User {
+    private String firstName;
+    private String lastName;
+    private Integer idCardNumber;
+    private Integer phoneNumber;
+    private String email;
+    private String password;
+    private Integer pin;
+    private Bitmap profilePhoto;
+    private String idCardPhoto;
+    private String userWithIdCardPhoto;
+    private String rooms;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getIdCardNumber() {
+        return idCardNumber;
+    }
+
+    public void setIdCardNumber(Integer idCardNumber) {
+        this.idCardNumber = idCardNumber;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getPin() {
+        return pin;
+    }
+
+    public void setPin(Integer pin) {
+        this.pin = pin;
+    }
+
+    public Bitmap getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(Bitmap profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public String getIdCardPhoto() {
+        return idCardPhoto;
+    }
+
+    public void setIdCardPhoto(String idCardPhoto) {
+        this.idCardPhoto = idCardPhoto;
+    }
+
+    public String getUserWithIdCardPhoto() {
+        return userWithIdCardPhoto;
+    }
+
+    public void setUserWithIdCardPhoto(String userWithIdCardPhoto) {
+        this.userWithIdCardPhoto = userWithIdCardPhoto;
+    }
+
+    public String getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(String rooms) {
+        this.rooms = rooms;
+    }
 }
