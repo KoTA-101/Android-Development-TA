513aabb97de6ea96d16aafb35d441ea7a104e898
Rohmat Dasuki <dasukirohmat@gmail.com>
2019-06-27 18:35:19 +0700

    update

--- InnStant_v0/app/src/main/java/com/example/innstant/ui/Auth/LoginActivity.java
+++ InnStant_v0/app/src/main/java/com/example/innstant/ui/Auth/LoginActivity.java
@@ -13,12 +13,22 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.innstant.R;
 import com.example.innstant.data.PreferenceHelper;
+import com.example.innstant.data.model.comment;
+import com.example.innstant.data.model.user2;
 import com.example.innstant.ui.DashboardActivity;
 import com.example.innstant.viewmodel.LoginViewModel;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.ButterKnife;
 
@@ -53,24 +63,42 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                username.getText().toString();
-                password.getText().toString();
+
+
                 Login();
             }
         });
 
     }
-    private void Login() {
-        mViewModel.openServerConnection();
+
+
+    public void Login(){
+        // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = PreferenceHelper.getBaseUrl() + "/users";
+        Gson gson = new GsonBuilder().create();
+
+// Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                response -> Toast.makeText(LoginActivity.this, "Users: " + response, Toast.LENGTH_LONG).show(),
-                error -> Toast.makeText(LoginActivity.this, error.getMessage(), Toast.LENGTH_LONG).show());
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                       // ArrayList<comment> coment = gson.fromJson(response, new TypeToken<List<comment>>(){}.getType());
+                        Toast.makeText( LoginActivity.this,response,Toast.LENGTH_LONG).show();
+                       // Toast.makeText( LoginActivity.this,  username.getText().toString() + password.getText().toString(),Toast.LENGTH_LONG).show();
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText( LoginActivity.this, error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+// Add the request to the RequestQueue.
         queue.add(stringRequest);
-        if(true){
-            Intent intent= new Intent(LoginActivity.this, DashboardActivity.class);
-            startActivity(intent);
-        }
+
+
     }
 }
